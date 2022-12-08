ab1a1636973757e6d8f5191a62602d59408961bc
npakku <pascal.naa.zury@gmail.com>
2022-12-08 11:31:23 -0800

    print text without using methods in material

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Result: Always 0 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 58);
+	return (1);
+}
