d5314a7c8fb0a28aa01f52ddb3a971669fdedccd
npakku <pascal.naa.zury@gmail.com>
2022-12-08 11:34:16 -0800

    print text

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Result: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" -Dora Korpa, 2015-10-19\n", 58);
 	return (1);
 }
