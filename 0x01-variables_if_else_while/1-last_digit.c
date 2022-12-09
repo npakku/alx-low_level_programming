3a1bffe05f40e3cc72e7874040ad4154731674d8
npakku <pascal.naa.zury@gmail.com>
2022-12-09 07:21:16 -0800

    test last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (last == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n, last);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 	return (0);
 
