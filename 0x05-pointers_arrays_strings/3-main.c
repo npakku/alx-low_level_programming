3698d82e96c43857c736523df3e63697befb4882
npakku <pascal.naa.zury@gmail.com>
2022-12-19 04:08:19 -0800

    print some strings

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
+
