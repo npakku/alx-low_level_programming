e8493ccf8f720dd0710080a722adfb5c8b462aae
npakku <pascal.naa.zury@gmail.com>
2022-12-21 04:11:08 -0800

    test number to see if it is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests integer to see if it is positive
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
