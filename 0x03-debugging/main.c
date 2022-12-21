776ef64c8533fd0614bccff10b75875b7d735399
npakku <pascal.naa.zury@gmail.com>
2022-12-21 03:09:02 -0800

    tests function

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - tests function 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * positive_or_negative - tests the number
+ * @n: the input
+ * Return: Always 0 (success)
+ */
+
+void positive_or_negative(int n)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+}
