22a70b5e04d6176c567478f71d9e2c61a0927514
npakku <pascal.naa.zury@gmail.com>
2022-12-30 02:24:28 -0800

    Fizz Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,22 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("%s", "FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s", "Buzz");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s", "Fizz");
+			printf("Fizz");
 		}
 		else
 		{
 			printf("%d", i);
 		}
-		printf(" ");
+		putchar(' ');
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
