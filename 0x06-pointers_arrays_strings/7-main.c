d3d17e5de89c3a203bc1747e57a8c06d474f3020
npakku <pascal.naa.zury@gmail.com>
2022-12-30 00:12:06 -0800

    encode a message

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char s[] = "Expect the best.Prepare for the worst.Capitalize on what comes\n";
+	char *p;
+
+	p = leet(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
+}
