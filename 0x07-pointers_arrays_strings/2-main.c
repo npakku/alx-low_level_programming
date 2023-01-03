c5565d2d3f9f2cc4ff250416db39281a29975dfb
npakku <pascal.naa.zury@gmail.com>
2023-01-03 07:16:43 -0800

    find occurence of character in string

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, 'l');
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+	return (0);
+}
