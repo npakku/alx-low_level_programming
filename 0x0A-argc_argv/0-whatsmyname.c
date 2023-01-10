35edd443c3ced0153bbade12fd803e56580f9c18
npakku <pascal.naa.zury@gmail.com>
2023-01-10 05:58:00 -0800

    print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: the count of command line arguments
+ * @argv: the array of command line arguments
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
