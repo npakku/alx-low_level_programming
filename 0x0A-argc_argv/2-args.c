b2d6f6845a2052087713a2586458488f12512b77
npakku <pascal.naa.zury@gmail.com>
2023-01-14 01:14:35 -0800

    print all arguments on the command line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return  (0);
+}
