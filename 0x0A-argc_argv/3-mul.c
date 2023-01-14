f24b5e82c44e69cdc4fd98f3b51861d3fb49c5c1
npakku <pascal.naa.zury@gmail.com>
2023-01-14 01:30:16 -0800

    multiply command line arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+	
+	argc = argc;
+	if (argv[1] == NULL || argv[2] == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
+	return (0);
+
+}
