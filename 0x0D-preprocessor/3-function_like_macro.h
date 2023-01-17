31d2c4dcbd55e2fa3dfef2af8bbb623653998938
npakku <pascal.naa.zury@gmail.com>
2023-01-17 15:51:32 -0800

    function line macor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,13 @@
-#ifndef ABS
-#define ABS(x) ((x) < (0) ? -(x) : (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * @x: the number to check
+ * Auth: Pascal
+ * Desc: Header file that defines a function-like macro ABS(x)
+ */
+
+#define ABS(x) ((x) < (0) ? (-x) : (x))
 
 #endif
