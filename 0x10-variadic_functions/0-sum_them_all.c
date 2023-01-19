2e5580a4fe41ae09782db10dc3dfd43e4fb50930
npakku <pascal.naa.zury@gmail.com>
2023-01-19 02:07:32 -0800

    sum elements in variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all the arguments of the function
+ * @n: the last named argument
+ *
+ * Return: Always 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list data;
+	unsigned int i;
+	int sum;
+
+	sum = 0;
+	va_start(data, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+			sum = sum + va_arg(data, int);
+	}
+	else
+	{
+		return (0);
+	}
+	return (sum);
+}
