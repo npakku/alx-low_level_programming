f1709deb03ada1f53923c50d6ed4fc3ad7243a7e
npakku <pascal.naa.zury@gmail.com>
2023-01-20 00:49:21 -0800

    print strings

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print some strings
+ * @separator: pointer to constant
+ * @n: number of arguments
+ *
+ * Return: Always 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *words;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		words = va_arg(ap, char *);
+		(words) ? printf("%s", words) : printf("(nill)");
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(ap);
+	printf("\n");
+}
