e87601e7a8178489f250449f50f7780226825a4a
npakku <pascal.naa.zury@gmail.com>
2023-01-20 00:58:22 -0800

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,12 +20,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		words = va_arg(ap, char *);
-		(words) ? printf("%s", words) : printf("(nill)");
+		(words) ? printf("%s", words) : printf("(nil)");
 		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
 	}
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }
