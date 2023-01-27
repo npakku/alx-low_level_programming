16c703fec00c1a4186682a9b1f1457573bfe242d
npakku <pascal.naa.zury@gmail.com>
2023-01-26 16:12:32 -0800

    count elements in linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - counts all the elements of a list_t list
+ * @h: input head of singly linked list
+ *
+ * Return: Number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
