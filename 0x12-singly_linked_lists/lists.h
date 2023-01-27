e62e3f51eea7d6a1e521f0236b3af8d635c776dd
npakku <pascal.naa.zury@gmail.com>
2023-01-26 16:22:57 -0800

    function that add new node at beginning of list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,13 +25,23 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* function prototypes */
-
+/* Function Prototypes */
+/* declaration of function to print all elements of a list_t list */
 size_t print_list(const list_t *h);
+
+/* declaration of function to count all elements of a list_t list */
 size_t list_len(const list_t *h);
+
+/* declaration of function to add a node to the beginning of a list_t list */
 list_t *add_node(list_t **head, const char *str);
+
+/* declaration of function to add a node to the end of a list_t list */
 list_t *add_node_end(list_t **head, const char *str);
+
+/* declaration of function to determine length of string */
+int string_length(const char *s);
+
+/* declaration of function to free a list_t list */
 void free_list(list_t *head);
-int _putchar(char c);
 
 #endif
