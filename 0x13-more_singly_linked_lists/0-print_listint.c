5b73a991bf7658eb074196aac89fc97d315ff07a
npakku <pascal.naa.zury@gmail.com>
2023-01-30 03:54:07 -0800

    print elements of linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints elements in linked list
+ * @h: pointer to structure
+ *
+ * Return: nothing
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		printf("List is empty");
+	}
+
+	while(h != NULL)
+	{
+		count = count + 1;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
